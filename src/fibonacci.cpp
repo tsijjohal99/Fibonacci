b9df9d22c5da08a743098ff73a6fdb8ae67b9732
Jeevan Johal <j.johal@softwareinstitute.com>
2022-04-01 09:40:25 +0100

    more efficient

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -3,10 +3,8 @@
 
 namespace fibonacci {
     int fibonacci(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
+        if ((n == 0) || (n == 1)) {
+            return n;
         } else {
             return fibonacci(n-1) + fibonacci(n-2);
         }
